7e81a5b8f01c0bea4fc3e795803ab57c95892f16
haramurti <134207028+haramurti@users.noreply.github.com>
2024-09-09 19:21:04 +0700

    Add files via upload

---
+++ encapsule/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Student c = new Student();
+        c.setName("hamaguchi");
+        System.out.println(c.getName());
+
+        c.setAge(40);
+        System.out.println(c.getAge());
+
+
+    }
+}
